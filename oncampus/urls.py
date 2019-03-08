2773a60114afb0d512700720f2fcd319ea23d0ba
Vaibhav <vaibhavjcsingh@gmail.com>
2019-03-08 16:37:18 +0530

    Student Home and Faculty Home

--- oncampus/urls.py
+++ oncampus/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('student/', include('students.urls')),
     path('faculty/', include('faculty.urls')),
-    path('/faculty_home/', home),
+    path('faculty_home/', home),
 ]
